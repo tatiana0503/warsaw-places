a17a3ed4e6e2521602fffe6af709359e8fd92b58
tatiana0503 <tzhamoitina@gmail.com>
2024-04-15 15:06:56 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
-import logo from './logo.svg';
+import { useState } from 'react';
+import { data } from './data';
 import './App.css';
 
 function App() {
+  const [place, setPlace] = useState(data);
+
+  const removePlace = (index) => {
+    let newPlace = place.filter(element => element.index !== index);
+    setPlace(newPlace);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className='container'>
+        <h1>TOP {place.length} PLACES TO VISIT IN WARSAW</h1>
+      </div>
+      {place.map((item => {
+        const {index, name, image} = item;
+        return (
+          <div key={index}>
+            <div className='container'>
+              <h2>{index} - {name}</h2>
+            </div>
+            <div className='container'>
+              <img src={image} width="500px" alt="place"/>
+            </div>
+            <div className='container'>
+              <button onClick={() => removePlace(index)}>REMOVE</button>
+            </div>
+          </div>
+        )
+      }))}
+      <div className='container'>
+        <button onClick={() => setPlace([])}>DELETE ALL</button>
+        </div>
+      
     </div>
   );
 }
---
+++ src/data.js
@@ -0,0 +1,31 @@
+export const data = [
+    {
+        index: 1,
+        name:'Old Town Market Square',
+        image:'https://assets.traveltriangle.com/blog/wp-content/uploads/2018/11/Old-Town-Market-Square1.jpg'
+    },
+
+    {
+        index: 2,
+        name: 'The Royal Route',
+        image:'http://imgur.com/UkE8A5k.jpg'
+    },
+
+    {
+        index: 3,
+        name:'The Royal Castle',
+        image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/9/90/POL_Warsaw_Royal_Castle_2008_%283%29.JPG/1024px-POL_Warsaw_Royal_Castle_2008_%283%29.JPG'
+    },
+
+    {
+        index: 4,
+        name:'Palace of Culture and Science',
+        image:'http://imgur.com/SmlMeKh.jpg'
+    },
+
+    {
+        index: 5,
+        name:'Lazienki Park',
+        image:'https://assets.traveltriangle.com/blog/wp-content/uploads/2018/11/Lazienki-Park_.jpg'
+    },
+]
